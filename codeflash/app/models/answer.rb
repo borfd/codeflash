d6bea31796e7e215dc042b711e32b32dcf0f9e4e
Boris Filipov <bfilipov@gmail.com>
2013-12-05 13:23:17 +0100

    Answer specs passing.

--- codeflash/app/models/answer.rb
+++ codeflash/app/models/answer.rb
@@ -2,4 +2,9 @@ require "ninja_sandbox/runnable_answer"
 
 class Answer < ActiveRecord::Base
 	include NinjaSandbox::RunnableAnswer
+
+	validates_presence_of :code
+	validates_presence_of :result
+
+	belongs_to :flashcard
 end
--- codeflash/spec/models/answer_spec.rb
+++ codeflash/spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Answer do
   it { should respond_to(:verify) }
-  it { should belong_to(:organization) }
+  it { should belong_to(:flashcard) }
   it { should validate_presence_of(:code) }
   it { should validate_presence_of(:result) }
 end
