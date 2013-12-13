7b5f03d6bd21c6f65c6228de8153f40ab7774d69
Boris Filipov <bfilipov@gmail.com>
2013-12-13 10:38:04 +0100

    CSS, answer should store result spec.

--- codeflash/spec/ninja_sandbox/answer_verifier_spec.rb
+++ codeflash/spec/ninja_sandbox/answer_verifier_spec.rb
@@ -24,5 +24,15 @@ describe NinjaSandbox::AnswerVerifier do
 		NinjaSandbox::AnswerVerifier.run(@answer.id).should eq(@answer)
 		@answer.correct.should be_false
 	end
+
+	it "should store answer after verification" do
+		Answer.stub(:find)
+			.with(@answer.id)
+			.and_return(@answer)
+		expect {
+		NinjaSandbox::AnswerVerifier.run(@answer.id)
+
+		}.to change(@answer.result).to("4")
+	end
   
 end
